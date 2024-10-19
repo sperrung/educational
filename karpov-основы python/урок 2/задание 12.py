4824fc4e978ad1693be039a79590b2f689280ec1
sperrung <daria.tyutrina@gmail.com>
2024-10-19 23:11:14 +0800

    python 2 lesson

---
+++ karpov-основы python/урок 2/задание 12.py
@@ -0,0 +1,34 @@
+#В переменной x хранится целое число.
+#  Проверьте, делится ли это число без остатка на 3 и на 7. 
+# Результат сохраните в переменную result. Переменная result должна иметь логический тип данных. 
+
+Если x = 7, то  result = False. Если x = 21, то result = True. 
+
+#Ваше решение
+Ошибка: invalid syntax (<string>, line 1)
+
+if (x % 3 == 0)
+and (x % 7 == 0)
+print (result = True)
+else 
+print (result = False)
+
+
+Тест, на котором получена ошибка
+
+x = 0
+
+Ошибка: invalid syntax (<string>, line 1)
+
+#Ваше решение
+
+if x % 3 == 0
+and x % 7 == 0
+print (result = True)
+else 
+print (result = False)
+
+
+Тест, на котором получена ошибка
+
+x = 0
