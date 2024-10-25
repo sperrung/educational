247f886a1411d8f3e1d27432226bb92f610ea0a2
sperrung <daria.tyutrina@gmail.com>
2024-10-26 07:44:55 +0800

    lesson 3 tasks added

---
+++ karpov-основы python/урок 3/задание 7.py
@@ -0,0 +1,5 @@
+#Найдите первый индекс числа 11 в my_list, сохраните индекс в переменную eleven_index.  
+#В переменную ten_count сохраните, сколько раз в списке my_list встречается число 10.
+
+eleven_index = my_list.index(11)
+ten_count = my_list.count(10)
